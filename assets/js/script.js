986794aef4ea94d28a71c7a27f2d078c968fbe5d
Jasmine Chamblin <jasminesimone.jc@gmail.com>
2022-09-04 10:47:45 -0700

    new styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,38 @@
-var searchFormEl = document.querySelector('#search-form');
+var tableBody = document.getElementById('repo-table');
+var fetchButton = document.getElementById('fetch-button');
 
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
+function getApi() {
+    // fetch request gets a list of all the repos for the node.js organization
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=chandler&appid=11a69bdecfa361ac52d7de87b1f518d9';
 
-  var searchInputVal = document.querySelector('#search-input').value;
-  var formatInputVal = document.querySelector('#format-input').value;
 
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
-  }
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
 
-  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+            var requestUrl2 = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&appid=11a69bdecfa361ac52d7de87b1f518d9';
+            fetch(requestUrl2)
+                .then(function (data2) {
+                    for (var i = 0; i < 6; i++) {
+                     var temp=document.getElementById(`temp${i+1}`)
+                    temp.innerText="temp"+i
+                    var humid=document.getElementById(`humid${i+1}`)
+                    humid.innerText="humid"+i
+                    var wind=document.getElementById(`wind${i+1}`)
+                    wind.innerText="wind"+i
+                    var uv=document.getElementById(`uv${i+1}`)
+                    uv.innerText="uv"+i
+                    var icon=document.getElementById(`icon${i+1}`)
+                    icon.src="icon"+i
+                    }
 
-  location.assign(queryString);
+                })
+
+
+        });
 }
 
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+fetchButton.addEventListener('click', getApi);
